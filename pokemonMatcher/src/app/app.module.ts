136fa6f0b01f8e8b84c564a75d3391422b3d6397
Noah Delano <delanonoah@gmail.com>
2020-11-02 10:17:57 -0700

    check

--- pokemonMatcher/src/app/app.module.ts
+++ pokemonMatcher/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from '../modules/material.module'
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from '../modules/app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/login/login.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { LobbyComponent } from './components/lobby/lobby.component';
+import { GameRoomComponent } from './components/game-room/game-room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserProfileComponent,
+    LobbyComponent,
+    GameRoomComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
