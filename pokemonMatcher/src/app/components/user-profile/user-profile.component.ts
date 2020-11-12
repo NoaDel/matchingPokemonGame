71b688c135f5878adfc0ca1cd912eb36b9733e26
Tylyn Adams <tyadams73@gmail.com>
2020-11-12 07:36:33 -0700

    user profile

--- pokemonMatcher/src/app/components/user-profile/user-profile.component.ts
+++ pokemonMatcher/src/app/components/user-profile/user-profile.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
-
+import { User } from '../../interfaces/user'
+import { AngularFirestore, AngularFirestoreDocument, DocumentChangeAction, DocumentSnapshot } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
@@ -13,19 +15,29 @@ export class UserProfileComponent implements OnInit {
   gamesLost: number;
   playersWonAgainst: string;
   playersLostAgainst: string;
-
-  user: firebase.User;
+  user: User
+  public getUser: AngularFirestoreDocument<User>
+  fbuser: firebase.User;
   constructor(
     private auth: AuthService,
+    private db: AngularFirestore,
     private router: Router
-  ) { }
-
+  ) {
+  }
   ngOnInit(): void {
     this.auth.getUserState()
-    .subscribe(user => {
-      this.user = user;
+    .subscribe(fbuser => {
+      this.fbuser = fbuser;
+      this.getUser = this.db.doc<User>(`Users/${this.fbuser.uid}`)
+      this.getUserObservable().subscribe(user => {
+        this.user = user
+        console.log(user)
+      });
+      console.log(fbuser)
     });
   }
-
+  getUserObservable(): Observable<User> {
+    return this.getUser.valueChanges()
+  }
 }
 
